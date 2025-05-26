8a5ae0defff51788509cb22fe922087fbb643b80
Soniauwitonze <uwitonzesonia2005gmail.com>
2025-05-27 01:27:44 +0300

    fifth commit

--- 01-fundamentals.js
+++ 01-fundamentals.js
@@ -1,9 +1,9 @@
-let num1 = 40;
-let num2 = 60;
-let num3 = 10;
-let number = [num1 , num2, num3];
-number.sort((a,b) => a - b);{
-  console.log("the sorted numbers are:", number);
+for( i = 0 ; i <= 15 ; i++) {
+  if (i % 2 === 0){
+    console.log(i+"i is even");
+  } else {
+    console.log(i+"i is odd");
+  }
 }
 
 
