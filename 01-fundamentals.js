9a0518cc92eaa8417e6cd9df0922b479747d0506
Soniauwitonze <uwitonzesonia2005gmail.com>
2025-05-26 17:08:42 +0300

    second commit

--- 01-fundamentals.js
+++ 01-fundamentals.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const num1 = 30
+const num2 = 40
+if (num1 > num2) {
+  console.log("num1 is greater than num2");
+} else if (num1 < num2) {
+  console.log("num1 is less than num2");
+} else {
+  console.log("num1 is equal to num2");
+}
