211577aad024bd544c7cd3532c35312c401189ee
Soniauwitonze <uwitonzesonia2005gmail.com>
2025-05-28 18:51:21 +0300

    for loop

--- 01-fundamentals.js
+++ 01-fundamentals.js
@@ -1,20 +1,13 @@
-var Students = [['David', 80], ['Vinoth', 77], ['Divya', 88], ['Ishitha', 95], ['Thomas', 68]];
-var totalmarks = 0;
-for (var i = 0 ; i < Students.length ; i++) {
-  totalmarks += Students[i][1];
-}
-avgmarks = totalmarks / Students.length ;
-if (avgmarks <= 100) {
-  console.log("Grade : A");
-} else if (avgmarks < 90) {
-  console.log("Grade : B");
-} else if (avgmarks < 80) {
-  console.log("Grade : C");
-} else if (avgmarks < 70) {
-  console.log("Grade : D");
+for (i = 0; i <= 100; i ++) {
+if (i % 3 === 0 && i % 5 === 0) {
+  console.log(i + "fizzbuzz");
+} else if (i % 3 === 0) {
+  console.log(i + "fizz");
+} else if (i % 5 === 0) {
+  console.log(i +  "buzz");
 } else {
-  console.log("Grade: F");
+  console.log(i);
 }
-
+}
 
 
