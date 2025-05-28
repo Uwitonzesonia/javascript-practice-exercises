3c460c7668e0e16ed086bfb38f0e7e4cfa0b6d34
Soniauwitonze <uwitonzesonia2005gmail.com>
2025-05-28 03:03:21 +0300

    average mark

--- 01-fundamentals.js
+++ 01-fundamentals.js
@@ -1,8 +1,19 @@
-var num = 10;
-if (num > 0) {
-  console.log(" num is positive");
+var Students = [['David', 80], ['Vinoth', 77], ['Divya', 88], ['Ishitha', 95], ['Thomas', 68]];
+var totalmarks = 0;
+for (var i = 0 ; i < Students.length ; i++) {
+  totalmarks += Students[i][1];
+}
+avgmarks = totalmarks / Students.length ;
+if (avgmarks <= 100) {
+  console.log("Grade : A");
+} else if (avgmarks < 90) {
+  console.log("Grade : B");
+} else if (avgmarks < 80) {
+  console.log("Grade : C");
+} else if (avgmarks < 70) {
+  console.log("Grade : D");
 } else {
-  console.log("num is negative");
+  console.log("Grade: F");
 }
 
 
